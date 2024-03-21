756fef121828d216aae6ca1b24ac5ad7b39355d3
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-03-21 15:02:55 -0500

    Update producto_api.sh

--- producto_api.sh
+++ producto_api.sh
@@ -4,6 +4,6 @@ cd apiProductos
 
 docker build -t productoapi .
 
-docker run -t -d -p 2018:2018 --name productoapirunning productoapi
+docker run -t -d -p 5445:5445 --name productoapirunning productoapi
 
 docker ps -a
